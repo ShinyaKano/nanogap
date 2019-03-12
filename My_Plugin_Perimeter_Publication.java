79926254504c011d7ee14f56b548161eba35c130
ShinyaKano <48478348+ShinyaKano@users.noreply.github.com>
2019-03-12 20:52:39 +0900

    Update My_Plugin_Perimeter_Publication.java

--- My_Plugin_Perimeter_Publication.java
+++ My_Plugin_Perimeter_Publication.java
@@ -5,23 +5,23 @@ import java.awt.*;
 import ij.plugin.*;
 import ij.plugin.frame.*;
 
-public class My_Plugin_Perimeter_Publication implements PlugIn {				//Calculate area, perimeter, and average nanogap distance using a binary converted image
+public class My_Plugin_Perimeter_Publication implements PlugIn {//Calculate area, perimeter, and average nanogap distance using a binary converted image
 
 	public void run(String arg) {
 		
-	      ImagePlus imp = IJ.getImage();
-      	ImageProcessor ip = imp.getProcessor();
-      	int width = ip.getWidth();
-      	int height = ip.getHeight();
+		ImagePlus imp = IJ.getImage();
+		ImageProcessor ip = imp.getProcessor();
+		int width = ip.getWidth();
+		int height = ip.getHeight();
 		int x=0;
 		int y=0;
 		int peri = 0;
 		int area = 0;
-		double pixtonm=(100.0/54.0); 										//pixel to nm conversion ratio: ((double)nm/(double)pixels)
+		double pixtonm=(100.0/54.0); 			//pixel to nm conversion ratio: ((double)nm/(double)pixels)
 
-		IJ.run(imp, "Open", "");									//Open (dilate and erode)
+		IJ.run(imp, "Open", "");			//Open (dilate and erode)
 
-		for (x=0;x<width;x++){										//Count area in pixels
+		for (x=0;x<width;x++){				//Count area in pixels
 			for (y=0;y<height;y++){
 				int intensity = ip.get(x,y);
 				if (intensity ==0){
